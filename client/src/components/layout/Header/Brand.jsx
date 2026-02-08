50909b3b8700ba6d5b272992e0a3275f9b9bd67c
Gyess <akhtarrauffasya@gmail.com>
2026-02-08 16:28:09 +0700

    Apa yaaa!?
    Hapus ui/Anchor.jsx dan membuat layer base untuk anchor sebagai gantinya.
    
    Buat layer base untuk body agar smooth-scroll.

--- client/src/components/layout/Header/Brand.jsx
+++ client/src/components/layout/Header/Brand.jsx
@@ -1,7 +1,11 @@
 function Brand() {
   return (
-    <div className="relative flex items-center hover:rotate-4 transition-all duration-200">
-      <img src="/astra-niaga.svg" alt="Logo Astra" className="w-12 md:w-16" />
+    <div className="group relative flex items-center transition-all duration-1000 hover:rotate-4">
+      <img
+        src="/astra-niaga.svg"
+        alt="Logo Astra"
+        className="relative w-12 transition-all duration-1000 group-hover:rotate-180 md:w-16"
+      />
       <p className="w-min text-2xl leading-none font-bold whitespace-pre-wrap text-blue-500 md:text-3xl">
         Astra Niaga
       </p>
--- client/src/components/ui/Anchor.jsx
+++
@@ -1,9 +0,0 @@
-function Anchor({ children }) {
-  return (
-    <span className="cursor-pointer transition-all duration-500 hover:text-blue-500">
-      {children}
-    </span>
-  );
-}
-
-export default Anchor;
